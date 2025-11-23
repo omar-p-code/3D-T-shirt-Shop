98c922b5a9d73a1651d597f9392ad5c66b326d16
omar <omaraldyb2020@gmail.com>
2025-11-24 00:27:03 +0200

    setting up the project

---
+++ client/src/canvas/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function canvas() {
+   return (
+      <div>Canvas</div>
+   )
+}
+
+export default canvas
---
+++ client/src/components/index.js
@@ -0,0 +1,13 @@
+import CustomButton from "./CustomButton";
+import FilePicker from "./FilePicker";
+import AIPicker from "./AIPicker";
+import ColorPicker from "./ColorPicker";
+import Tab from "./Tab";
+
+export {
+   CustomButton,
+   FilePicker,
+   AIPicker,
+   ColorPicker,
+   Tab
+};
